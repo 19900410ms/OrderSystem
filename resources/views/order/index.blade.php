931bd3f9b6dca2001d0e7843d666fd368201389c
19900410ms <57243729+19900410ms@users.noreply.github.com>
2020-03-24 15:22:15 +0900

    Order/index 合計金額の表示

--- resources/views/order/index.blade.php
+++ resources/views/order/index.blade.php
@@ -1,13 +1,37 @@
+<?php
+  $total_price = 0;
+  foreach ($orders as $order) {
+    $single_price = $order->menu->price * $order->count;
+    $total_price += $single_price;
+  }
+?>
 @extends('layouts.app')
 
 @section('content')
 
 <div class="container">
-  index
-  @foreach($orders as $order)
-    {{ $order->count }}
-    {{ $order->menu->name }}
-  @endforeach
+  <div class="row">
+    @foreach($orders as $order)
+      <?php $total = $order->menu->price * $order->count ?>
+      <div class="card w-50">
+        <div class="card-body">
+          <h5 class="card-title">{{ $order->menu->name }}</h5>
+          <p class="card-text">{{ $order->menu->price }} yen × {{ $order->count }}</p>
+          <h5 class="card-title">{{ $total }} yen</h5>
+        </div>
+      </div>
+    @endforeach
+  </div>
+
+  <div class="card text-center">
+    <div class="card-header">
+      Total Price
+    </div>
+    <div class="card-body">
+      <h5 class="card-title">{{ $total_price }}</h5>
+      <a href="#" class="btn btn-primary">お会計へ</a>
+    </div>
+  </div>  
 </div>
 
 @endsection
