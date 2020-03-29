660c1266f60a9e3989e1c1a9251542ddd069cde5
19900410ms <57243729+19900410ms@users.noreply.github.com>
2020-03-29 19:55:11 +0900

    画像表示確認

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -4,21 +4,21 @@
 <div class="container">
   <div class="card-columns">
     <div class="card">
-      <img class="card-img-top" src="../storage/meat.jpg" height="220px" width="100%" alt="Meats">
+      <img class="card-img-top" src="../storage/app/public/meat.jpg" height="220px" width="100%" alt="Meats">
       <div class="card-body">
         <h5 class="card-title">Meats</h5>
         <a href="{{ route('menu.index', ['category' => '1']) }}" class="btn btn-primary">メニューを確認する</a>
       </div>
     </div>
     <div class="card">
-      <img class="card-img-top" src="../storage/fish.jpg" height="220px" width="100%" alt="Fishes">
+      <img class="card-img-top" src="/storage/app/public/fish.jpg" height="220px" width="100%" alt="Fishes">
       <div class="card-body">
         <h5 class="card-title">Fishes</h5>
         <a href="{{ route('menu.index', ['category' => '2']) }}" class="btn btn-primary">メニューを確認する</a>
       </div>
     </div>
     <div class="card">
-      <img class="card-img-top" src="../storage/salada.jpg" height="220px" width="100%" alt="Saladas">
+      <img class="card-img-top" src="storage/app/public/salada.jpg" height="220px" width="100%" alt="Saladas">
       <div class="card-body">
         <h5 class="card-title">Salads</h5>
         <a href="{{ route('menu.index', ['category' => '3']) }}" class="btn btn-primary">メニューを確認する</a>
