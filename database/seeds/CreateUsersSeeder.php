4b65612944bdedd132fce21087a556e14d761da6
19900410ms <57243729+19900410ms@users.noreply.github.com>
2020-03-29 11:45:24 +0900

    シーダー追加・Viewの微調整

--- database/seeds/CreateUsersSeeder.php
+++ database/seeds/CreateUsersSeeder.php
@@ -16,20 +16,32 @@ class CreateUsersSeeder extends Seeder
             [
                'table_number'=>'666',
                'email'=>'admin@itsolutionstuff.com',
-                'is_admin'=>'1',
+               'is_admin'=>'1',
                'password'=> bcrypt('123456'),
             ],
             [
                'table_number'=>'1',
                'email'=>'user@itsolutionstuff.com',
-                'is_admin'=>'0',
+               'is_admin'=>'0',
                'password'=> bcrypt('123456'),
             ],
             [
-                'table_number'=>'2',
-                'email'=>'costomer@itsolutionstuff.com',
-                 'is_admin'=>'0',
-                'password'=> bcrypt('123456'),
+               'table_number'=>'2',
+               'email'=>'costomer@itsolutionstuff.com',
+               'is_admin'=>'0',
+               'password'=> bcrypt('123456'),
+             ],
+             [
+               'table_number'=>'3',
+               'email'=>'sample_user@itsolutionstuff.com',
+               'is_admin'=>'0',
+               'password'=> bcrypt('123456'),
+             ],
+             [
+               'table_number'=>'4',
+               'email'=>'sample_costomer@itsolutionstuff.com',
+               'is_admin'=>'0',
+               'password'=> bcrypt('123456'),
              ],
         ];
   
